f0f3b59250b3c03716a7b4fcd22ce2dbbd23ec36
Dhruv Saini <56490747+oof-baroomf@users.noreply.github.com>
2024-01-19 15:45:14 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 from flask import Flask, render_template, request, redirect
 import pollingDB
+import json
+from datetime import date
 
 app = Flask(__name__)
 
@@ -7,7 +9,9 @@ validResourcePaths:set = {"index/index.js", "index/style.css"}
 
 @app.route('/')
 def landingPage() -> str:
-    return render_template('index/index.html')
+    # open file w/ questions, get question for today
+    question = json.load(open("questions.json", "r"))[date.today()]
+    return render_template('page1/index.html', question = question)
     
 
 @app.route('/resource/<path:path>')
