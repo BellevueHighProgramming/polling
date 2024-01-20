408f45f41b67cd2b39c200a0a1415cc06f0abbae
Justin Jiang <justin.jiang.co@gmail.com>
2024-01-19 16:28:27 -0800

    dummy request data

---
+++ templates/page1/data.js
@@ -0,0 +1,16 @@
+export function getNumberOfOptions(){
+    return 0;
+}
+
+export function getQuestion(){
+    return "im a question"
+}
+
+export function listOfOptions(){
+    return [
+        "option 1 example",
+        "option 2 example",
+        "option 3 example",
+        "option 4 example",
+    ]
+}
---
+++ templates/page2/data.js
@@ -0,0 +1,12 @@
+export function numberOfOptions(){
+
+}
+export function optionsAndData(){
+    //RETURNS JSON FILE CONTAINING THE OPTIONS, AND HOW MANY PEOPLE SELECTED EACH OPTION
+    return{
+        "Option 1 example": 10,
+        "Option 2 example": 100,
+        "Option 3 example": 17,
+        "Option 4 example": 69,
+    }
+}
